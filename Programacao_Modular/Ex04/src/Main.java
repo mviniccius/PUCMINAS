56a6491f75adb5e8161b0ee86a1cac8c2a3adaa7
mviniccius <marcus.viniccius@hotmail.com>
2023-08-14 18:17:07 -0300

    Exercicio 4 da lista

--- Programacao_Modular/Ex04/src/Main.java
+++ Programacao_Modular/Ex04/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Declarando variaveis:
+        String frase;
+        String invertida;
+        
+        System.out.println("Inverter frase ou palavra");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.println("Digite uma frase ou 'fim'para sair");
+
+            frase = scanner.nextLine();
+
+            if(frase.equals("fim")){
+                System.out.println("Fim do programa");
+                break;
+            }
+            invertida = inverterFrase(frase);
+            System.out.println("A frase invertida: " + invertida);
+
+        }
+        scanner.close();
     }
+    public static String inverterFrase(String frase) {
+        StringBuilder sb = new StringBuilder(frase);
+        return sb.reverse().toString();
+    }
+
 }
