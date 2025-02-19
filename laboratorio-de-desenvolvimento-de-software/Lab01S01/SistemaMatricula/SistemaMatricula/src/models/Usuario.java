920df3d89c746a413f3ea146387131484b4d92ff
mviniccius <marcus.viniccius@hotmail.com>
2025-02-19 20:15:42 -0300

    create new java projet

---
+++ laboratorio-de-desenvolvimento-de-software/Lab01S01/SistemaMatricula/SistemaMatricula/src/models/Usuario.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Usuario {
+	protected String login;
+	protected String senha;
+	
+	public Usuario(String login, String senha) {
+		this.login = login;
+		this.senha = senha;
+	}
+}
