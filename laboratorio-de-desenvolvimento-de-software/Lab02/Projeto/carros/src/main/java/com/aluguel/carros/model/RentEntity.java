5004d024d8cefec632395de641e9ed243ed26201
mviniccius <marcus.viniccius@hotmail.com>
2025-03-28 19:28:34 -0300

    Ajuste no codigo(erro Merge)

--- laboratorio-de-desenvolvimento-de-software/Lab02/Projeto/carros/src/main/java/com/aluguel/carros/model/RentEntity.java
+++ laboratorio-de-desenvolvimento-de-software/Lab02/Projeto/carros/src/main/java/com/aluguel/carros/model/RentEntity.java
@@ -2,10 +2,32 @@ package com.aluguel.carros.model;
 
 import org.springframework.data.annotation.Id;
 
+import java.security.PrivateKey;
+
 public class RentEntity {
 
     @Id
     private String id;
+    private String userId;
+
+    private String carId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+
 
     public String getId() {
         return id;
