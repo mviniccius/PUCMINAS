4f4b4f1c722c50961a2477723281b676b1ad6e0b
mviniccius <marcus.viniccius@hotmail.com>
2025-03-30 09:37:45 -0300

    validacao para realizar login, back end de login pronto

--- laboratorio-de-desenvolvimento-de-software/Lab02/Projeto/carros/src/main/java/com/aluguel/carros/controller/UserController.java
+++ laboratorio-de-desenvolvimento-de-software/Lab02/Projeto/carros/src/main/java/com/aluguel/carros/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.aluguel.carros.controller;
 
 
+import com.aluguel.carros.dto.LoginRequest;
 import com.aluguel.carros.model.UserEntity;
+import com.aluguel.carros.repository.UserRepository;
 import com.aluguel.carros.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -14,6 +19,10 @@ public class UserController {
 
    @Autowired
     private UserService userService;
+
+   @Autowired
+   private UserRepository userRepository;
+
 
    @GetMapping
     public List<UserEntity>listarTodos(){
@@ -34,6 +43,21 @@ public class UserController {
    @PutMapping("/{id}")
     public UserEntity atualizar(@PathVariable String id, @RequestBody UserEntity user){
        return userService.atualizar(id, user);
+   }
+   @PostMapping("/login")
+    public ResponseEntity<?> autenticar(@RequestBody LoginRequest login){
+       Optional<UserEntity> usuario = userRepository.findByEmailESenha(
+               login.getEmail(),
+               login.getEmail()
+       );
+
+       if(usuario.isPresent()){
+           UserEntity u = usuario.get();
+           u.setSenha(null);
+           return ResponseEntity.ok(u);
+       }else {
+           return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Errou");
+       }
    }
 
 }
--- laboratorio-de-desenvolvimento-de-software/Lab02/Projeto/carros/src/main/java/com/aluguel/carros/repository/UserRepository.java
+++ laboratorio-de-desenvolvimento-de-software/Lab02/Projeto/carros/src/main/java/com/aluguel/carros/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.aluguel.carros.model.UserEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<UserEntity, String> {
+    Optional<UserEntity> findByEmailESenha(String email, String senha);
 }
