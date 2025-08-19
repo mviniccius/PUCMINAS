ded83d0d23899afae99f1a72fe465fccff9aeebf
mviniccius <marcus.viniccius@hotmail.com>
2025-08-19 17:40:32 -0300

    implementacao do isSimples

--- Teoria-dos-Grafos/Grafos-2/src/Main.java
+++ Teoria-dos-Grafos/Grafos-2/src/Main.java
@@ -1,35 +1,61 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         int maxVertice = 100;
         int [][] grafo = new int[maxVertice][maxVertice];
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(new File("in.txt"));
         int temp;
-        System.out.println("Digite o numero de veritices:");
-        temp = scanner.nextInt(); // ler o numero de vertices
-        maxVertice = temp;
+
+        while (true){
+          //leitura da primeira linha do arquivo:
+            //System.out.println("Digite o numero de veritices:");
+            temp = scanner.nextInt(); // ler o numero de vertices
+          //verificacao de saida do loop:
+            if(temp == 0){
+                break;
+            }
+            maxVertice = temp;
 
         for(int i = 0; i < maxVertice; i++){
-             grafo[0][0] = -1;
+             grafo[i][i] = 0;
              for (int j = i + 1; j < maxVertice; j++){
-                 System.out.println("Valor da Areste: ");
+                 //System.out.println("Valor da Aresta posicao: " + i + " " + " " + j);
                  temp = scanner.nextInt();
                  grafo[i][j] = temp;
                  grafo[j][i] = temp;
              }
-
+            }
+            imprimirMatriz(grafo,maxVertice);
+            System.out.println("isSimples: " + isSimples(grafo, maxVertice)); // + "isRegular: " + + "isCompleto "+ + "isNulo "+ + "isLinear "
         }
-        
-        //imprimir
-        for(int i = 0; i < maxVertice; i++){
-            for (int j = 0; j < maxVertice; j++){
-                System.out.print(grafo[i][j]+ " ");
+
+
+            scanner.close();
+    }
+
+    static void imprimirMatriz(int [][] m, int tamanhoVertice){
+        for(int i = 0; i < tamanhoVertice; i++){
+            for (int j = 0; j < tamanhoVertice; j++){
+                System.out.print(m[i][j]+ " ");
             }
             System.out.println();
         }
+    }
 
+    static boolean isSimples(int [][]m, int tamanhoVertice){
+
+        for (int i = 0; i < tamanhoVertice; i++){
+            if (m[i][i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    static boolean isRegular(int [][]m , int tamanhoVertice){
+
+        return true;
     }
 }
